b9515414cd43a0595b65d9226b5ad57f7c5af54b
kumarmanish562 <qmanishkumar562@gmail.com>
2025-03-21 13:22:19 +0530

    feat: add custom fonts and initial screen with centered text

---
+++ cooking.ai/app/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      
+      <Text style={{
+      fontSize: 30,
+      fontFamily: 'outfit-bold'
+      }}>Manish Kumar.</Text>
+    </View>
+  );
+}
